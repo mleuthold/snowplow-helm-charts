9a776f75f1052349785e569dedb88e9672657a20
Martin Leuthold <martinleuthold88+git@gmail.com>
2019-03-03 02:56:44 +0100

    Created Helm chart for Snowplow Scala collector for Kafka.

--- shell/install-kafka.sh
+++ shell/install-kafka.sh
@@ -3,4 +3,4 @@
 helm repo add confluentinc https://confluentinc.github.io/cp-helm-charts/
 #helm repo update
 
-helm tiller run "default" -- helm upgrade --install --wait --timeout 300 --namespace "default" "kafka" confluentinc/cp-helm-charts -f shell/kafka-values.yaml
+helm tiller run "default" -- helm upgrade --install --wait --timeout 600 --namespace "default" "kafka" confluentinc/cp-helm-charts -f shell/kafka-values.yaml
